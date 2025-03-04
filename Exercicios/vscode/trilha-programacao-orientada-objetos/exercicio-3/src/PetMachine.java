e70e2e4a9ffbea54bbd48da115dd64e5bdaf1044
Rafael Secco <raf.secco@gmail.com>
2025-03-04 09:50:57 -0300

    Add Pet and PetMachine classes with user interaction for pet care

---
+++ Exercicios/vscode/trilha-programacao-orientada-objetos/exercicio-3/src/PetMachine.java
@@ -0,0 +1,82 @@
+public class PetMachine {
+	private boolean clean = true;
+	private int water = 30;
+	private int shampoo = 10;
+	private Pet pet = null;
+	
+	public void takeAShower() {
+		if(this.pet == null) {
+			System.out.println("Coloque o pet na máquina para iniciar o bancho");
+			return;
+		}
+		
+		this.water -= 10;
+		this.shampoo -= 2;
+		
+		pet.setClean(true);
+		System.out.println("O pet " + pet.getName() + " está limpo.");
+	}
+	
+	public void addWater() {
+		if(water == 30) {
+			System.out.println("A capacidade de água da máquina está no máximo");
+			return;
+		}
+		
+		water += 2;
+	}
+	
+	public void addShampoo() {
+		if(shampoo == 10) {
+			System.out.println("A capacidade de shampoo da máquina está no máximo");
+			return;
+		}
+		
+		shampoo += 2;
+	}
+	
+	public int getWater() {
+		return water;
+	}
+	
+	public int getShampoo() {
+		return shampoo;
+	}
+	
+	public boolean hasPet() {
+		return pet != null;
+	}
+	
+	public void setPet(Pet pet) {
+		if(!this.clean) {
+			System.out.println("A máquina está suja, para colocar o pet é necessário limpa-la");
+			return;
+		}
+		
+		if(hasPet()) {
+			System.out.println("O pet " + this.pet.getName() + " está na máquina nesse momento");
+			return;
+		}
+		
+		this.pet = pet;
+		System.out.println("O pet " + pet.getName() + " foi colocado na máquina.");
+	}
+	
+	public void removePet() {
+		if (!hasPet()) { 
+			System.out.println("A máquina não tem pet para ser retirado.");
+			return;
+		}
+		this.clean = this.pet.isClean();
+		System.out.println("O pet " + this.pet.getName() + " foi tirado da máquina.");
+		this.pet = null;
+	}
+	
+	public void wash() {
+		this.water -= 10;
+		this.shampoo -= 2;
+		this.clean = true;
+		System.out.println("A máquina foi limpa.");
+	}
+
+}
