fa39deb1e4371e3116e464aaec4e0e993e9b8288
Rafael Secco <raf.secco@gmail.com>
2025-03-15 18:41:04 -0300

    refactor UserRepository and StartApp to enhance user retrieval methods and update user details

--- Projetos/4-desenvolvimento_APIs/aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
+++ Projetos/4-desenvolvimento_APIs/aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
@@ -2,6 +2,8 @@ package dio.aula;
 
 import dio.aula.model.User;
 import dio.aula.repository.UserRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,16 +14,42 @@ public class StartApp implements CommandLineRunner {
 	@Autowired
 	private UserRepository repository;
 
+	// @Override
+	// public void run(String... args) throws Exception {
+	// System.out.println("***** Criando Usuario
+	// ************************************************************");
+	// User user = new User();
+	// user.setName("Rafael");
+	// user.setUsername("rafael");
+	// user.setPassword("123");
+	// System.out.println("***** Salvando Usuario
+	// ************************************************************");
+	// repository.save(user);
+	// System.out.println("***** Exibindo Usuarios
+	// ************************************************************");
+	// for (User u : repository.findAll()) {
+	// System.out.println(u);
+	// }
+	// }
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("***** Criando Usuario ************************************************************");
+		System.out.println("**********************************************************************");
+		//List<User> users = repository.filtrarPorNome("Rafael");
+		List<User> users = repository.findByNameContaining("Rafael");
+		for (User u : users) {
+			System.out.println(u);
+		}
+		System.out.println("**********************************************************************");
+	}
+
+	private void insertUser() {
 		User user = new User();
-		user.setName("Rafael");
+		user.setName("Rafael Secco");
 		user.setUsername("rafael");
-		user.setPassword("123");
-		System.out.println("***** Salvando Usuario ************************************************************");
+		user.setPassword("rafael123");
 		repository.save(user);
-		System.out.println("***** Exibindo Usuarios ************************************************************");
+
 		for (User u : repository.findAll()) {
 			System.out.println(u);
 		}
