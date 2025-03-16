87cbcde2b19afbff951a3d24136b841306a3ccb7
Rafael Secco <raf.secco@gmail.com>
2025-03-16 14:36:04 -0300

    add custom exception handling for mandatory fields and global error responses

---
+++ Projetos/4-desenvolvimento_APIs/my-first-web-api/src/main/java/dio/my_first_web_api/handler/CampoObrigatorioException.java
@@ -0,0 +1,9 @@
+package dio.my_first_web_api.handler;
+
+public class CampoObrigatorioException extends BusinessException {
+
+	public CampoObrigatorioException(String campo) {
+		super("O campo %s é obrigatório", campo);
+	}
+
+}
--- Projetos/4-desenvolvimento_APIs/my-first-web-api/src/main/java/dio/my_first_web_api/repository/UsuarioRepository.java
+++ Projetos/4-desenvolvimento_APIs/my-first-web-api/src/main/java/dio/my_first_web_api/repository/UsuarioRepository.java
@@ -5,11 +5,24 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import dio.my_first_web_api.handler.BusinessException;
+import dio.my_first_web_api.handler.CampoObrigatorioException;
 import dio.my_first_web_api.model.Usuario;
 
 @Repository
 public class UsuarioRepository {
 	public void save(Usuario usuario) {
+
+		if(usuario.getLogin() == null) {
+			//throw new BusinessException("O campo login é obrigatório");
+			//throw new RuntimeException("O campo login é obrigatório");
+			throw new CampoObrigatorioException("login");
+		}
+
+		if(usuario.getPassword() == null) {
+			throw new CampoObrigatorioException("password");
+		}
+
 		if(usuario.getId() == null)
 			System.out.println("SAVE - Recebendo o usuário na camada de repositório");
 		else
