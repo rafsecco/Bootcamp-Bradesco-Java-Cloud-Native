cab8e5d6f503b3c36cc45e04296abf239cd04c08
Rafael Secco <raf.secco@gmail.com>
2025-03-31 07:34:34 -0300

    add initial database setup and entity classes for employee management

---
+++ Projetos/5-banco-dados/jdbc-sample/src/main/java/dio/persistence/entity/ModuleEntity.java
@@ -0,0 +1,16 @@
+package dio.persistence.entity;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ModuleEntity {
+
+	private long id;
+
+	private String name;
+
+	private List<EmployeeEntity> employees;
+
+}
